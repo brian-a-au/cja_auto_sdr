778d0438b54e4e0e6f04c5f490a1303866292fcb
Brian Au <brian.a.au@gmail.com>
2026-04-03 20:40:41 -0700

    fix: rename test_version_is_3_5_11 → test_version_is_3_5_12

--- tests/test_ux_features.py
+++ tests/test_ux_features.py
@@ -344,7 +344,7 @@ class TestCombinedFeatures:
 class TestVersionUpdated:
     """Test that version is correct"""
 
-    def test_version_is_3_5_11(self):
+    def test_version_is_3_5_12(self):
         """Test that version is 3.5.12"""
         from cja_auto_sdr.generator import __version__
 
